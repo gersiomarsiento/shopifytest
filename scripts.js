b935bb93243207e712adfbf5b44554f4331f1b3c
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-10-17 11:00:14 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper(".swiper-hero", {
+  direction: "horizontal",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-nex",
+    prevEl: ".swiper-button-pre",
+  },
+});
+
+const swiperProducts = new Swiper(".swiperProducts", {
+  slidesPerView: "auto",
+  slidesPerGroup: 5,
+  loopFillGroupWithBlank: true,
+  loop: false,
+  direction: "horizontal",
+  pagination: {
+    el: ".swiper-pagination-products",
+  },
+});
+
+const groupButtons = document.querySelectorAll(".group-btn");
+groupButtons.forEach((button) => {
+  button.addEventListener("click", function (btn) {
+    groupButtons.forEach((btnn) => {
+      btnn.classList.remove("btn-active");
+    });
+    let number = this.dataset.number;
+    showCarousel(`group${number}`);
+    btn.target.classList.add("btn-active");
+  });
+});
+
+function showCarousel(groupId) {
+  const carousels = document.querySelectorAll(".group");
+  carousels.forEach((group) => {
+    group.classList.add("hidden");
+  });
+
+  const selectedGroup = document.getElementById(groupId);
+  if (selectedGroup) {
+    selectedGroup.classList.remove("hidden");
+  }
+}
+
+function showNav() {
+  document.getElementById("nav-ul").classList.toggle("hidden");
+}
